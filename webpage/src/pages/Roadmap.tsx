b32eb56477ec9bee7d32707b8bcc7867a7ce54c8
m.hollink <m.hollink@dictu.nl>
2024-09-26 21:04:38 +0200

    Update roadmap

--- webpage/src/pages/Roadmap.tsx
+++ webpage/src/pages/Roadmap.tsx
@@ -5,27 +5,39 @@ import Container from "@mui/material/Container";
 import { ExternalLink } from "@components/link/ExternalLink.tsx";
 import { Crumbs } from "@components/cumbs/Crumbs.tsx";
 
-const featureList = {
-  "Organizing your miniatures by army/faction":
-    "First things first; Saving your miniatures to the app.",
-  "Saving the current status of your miniatures":
-    "Insight in how many of you miniatures are still on the pile-of-shame can be a great motivation to continue for " +
-    "that last stretch of painting. Saving how far along you are can allows us to plot that data into some nice graphs.",
-  "Creating your own work flow steps":
-    "Each person paints in their own particular way. Do you base first then prime, or paint first and base you minis " +
-    "as a last step... being able to customize your workflow allows for a better insight in your current pile-of-shame.",
-  "Saving one (or multiple) paint scheme(s) per miniature":
-    "Whenever you are doing little bit of painting at a time, or just wanna remember your paint scheme " +
-    "in half a year when you buy some new minis. It would be great to be able to recall what paints you " +
-    "used.",
-  "Organizing your miniatures by game system":
-    "Having a big list of miniature groups can become overwhelming when you have " +
-    "multiple gaming systems. Allowing to further organize these groups into sets of IE. Middle-Earth, 40K, D&D, etc. " +
-    "allows for a smoother user experience",
-  "Personal accounts":
-    "Allowing users to create accounts or log in with their social media accounts. This will allow us to save data " +
-    "outside of the web application and eventually enable cross platform accounts.",
-};
+const features = [
+  {
+    done: true,
+    feature: "Organizing your miniatures by army/faction",
+    description: "First things first; Saving your miniatures to the app.",
+  },
+  {
+    done: true,
+    feature: "Saving the current status of your miniatures",
+    description:
+      "Insight in how many of you miniatures are still on the pile-of-shame can be a great motivation to continue for that last stretch of painting. Saving how far along you are can allows us to plot that data into some nice graphs.",
+  },
+  {
+    done: true,
+    feature: "Creating your own work flow steps",
+    description:
+      "Each person paints in their own particular way. Do you base first then prime, or paint first and base you minis as a last step... being able to customize your workflow allows for a better insight in your current pile-of-shame.",
+  },
+  {
+    feature: "Saving one (or multiple) paint scheme(s) per miniature",
+    description:
+      "Whenever you are doing little bit of painting at a time, or just wanna remember your paint scheme in half a year when you buy some new minis. It would be great to be able to recall what paints you used.",
+  },
+  {
+    feature: "Personal accounts",
+    description:
+      "Allowing users to create accounts or log in with their social media accounts. This will allow us to save data outside of the web application and eventually enable cross platform accounts.",
+  },
+  {
+    feature: "And more..",
+    description: "depending on request.",
+  },
+];
 
 export const Roadmap: FunctionComponent = () => {
   return (
@@ -80,9 +92,17 @@ export const Roadmap: FunctionComponent = () => {
           As for now, there is a lot to come before we will release the first
           1.0 version. Currently I have following features in mind:
           <ul>
-            {Object.entries(featureList).map(([feature, description]) => (
+            {features.map(({ feature, description, done }) => (
               <li key={feature}>
-                <strong>{feature}</strong>: {description}
+                {done ? (
+                  <s>
+                    <strong>{feature}</strong>: {description}
+                  </s>
+                ) : (
+                  <>
+                    <strong>{feature}</strong>: {description}
+                  </>
+                )}
               </li>
             ))}
           </ul>
