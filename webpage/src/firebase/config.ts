8d0f286538eb515fbcb114b36c9b2ae38899a3cc
m.hollink <m.hollink@dictu.nl>
2024-09-28 20:12:16 +0200

    Move the firebase secrets to an .env file

---
+++ webpage/src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  appId: import.meta.env.FIREBASE_APP_ID,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: "miniature-inventory.firebaseapp.com",
+  projectId: "miniature-inventory",
+  storageBucket: "miniature-inventory.appspot.com",
+  messagingSenderId: "300464295573",
+  measurementId: "G-JVWVWWMTL2",
+};
--- webpage/src/vite-env.d.ts
+++ webpage/src/vite-env.d.ts
@@ -1,3 +1,12 @@
 /// <reference types="vite/client" />
 declare const BUILD_VERSION: string;
 declare const BUILD_DATE: string;
+
+interface ImportMetaEnv {
+  readonly FIREBASE_API_KEY: string;
+  readonly FIREBASE_APP_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
