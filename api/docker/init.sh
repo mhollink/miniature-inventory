d9ffdad3d50f465e2532fc566639ae8e51f0875d
m.hollink <m.hollink@dictu.nl>
2024-10-12 22:45:22 +0200

    Make it possible to run the api in docker

---
+++ api/docker/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "DB_HOST=mariadb
+DB_PORT=3306
+DB_DATABASE=my_database
+DB_USERNAME=my_user
+DB_PASSWORD=my_password" > /var/www/html/.env
+
+apache2-foreground
--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -5,4 +5,12 @@ cd "$(cd "$(dirname "$0")" && pwd)";
 
 rm -rf ../release
 rsync -a ../webpage/dist/ ../release/
-rsync -a --exclude='.gitignore' --exclude='composer.json' --exclude='composer.lock' --exclude='Miniature Inventory.postman_collection.json' ../api ../release
+rsync -a \
+  --exclude='.gitignore' \
+  --exclude='composer.json' \
+  --exclude='composer.lock' \
+  --exclude='docker/' \
+  --exclude='Dockerfile' \
+  --exclude='apache.conf' \
+  --exclude='Miniature Inventory.postman_collection.json' \
+  ../api ../release
